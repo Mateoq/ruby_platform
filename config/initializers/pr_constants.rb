2f78bcbebdbb39205eb9abbe158f28da84003048
Mateo <quinterom1592@gmail.com>
2015-07-21 16:48:10 -0500

    Fixes

--- config/initializers/pr_constants.rb
+++
@@ -1,2 +0,0 @@
-GRADES = { primero: 01, segundo: 02, tercero: 03, cuarto: 04, quinto:05 }
-CLASSES = { mat: "Matemáticas", sci: "Ciencias Naturales", esp: "Lengua Castellana", soc: "Ciencias Sociales" }
